e3046fd1cf222ae40ce31c9f2ae0acf51ac40239
Дмитрий Каблучко <xequrt@yandex.ru>
2024-11-30 23:45:15 +1000

    Create template index.html

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,5 @@
-# from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
-from flask import Flask
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -9,9 +8,28 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
+url_data = {}
+
+
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def index():
+    return render_template('index.html', title='Анализатор страниц')
+
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
+    url = request.form['url']
+    url_id = len(url_data) + 1
+    url_data[url_id] = url
+    return redirect(url_for('url_detail', url_id=url_id))
+
+
+@app.route('/urls/<int:url_id>')
+def url_detail(url_id):
+    url = url_data.get(url_id)
+    if url is None:
+        return "URL не найден", 404
+    return render_template('url_detail.html', url=url, url_id=url_id)
 
 
 if __name__ == '__main__':
