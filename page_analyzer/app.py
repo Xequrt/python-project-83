76684c6cd59d757406271626cf9cf6759c738147
Дмитрий Каблучко <xequrt@yandex.ru>
2024-12-03 23:42:38 +1000

    Create database, analyzer_page

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import os
+import psycopg2
 from dotenv import load_dotenv
+import validators
+from urllib.parse import urlparse
+from datetime import datetime
 
 load_dotenv()
+DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret_key')
 
-
-url_data = {}
+def get_db_connection():
+    conn = psycopg2.connect(DATABASE_URL)
+    return conn
 
 
 @app.route('/')
@@ -19,18 +25,57 @@ def index():
 @app.route('/analyze', methods=['POST'])
 def analyze():
     url = request.form['url']
-    url_id = len(url_data) + 1
-    url_data[url_id] = url
-    return redirect(url_for('url_detail', url_id=url_id))
 
+    parsed_url = urlparse(url)
+    normalized_url = parsed_url.geturl()
+
+    if not validators.url(normalized_url) or len(normalized_url) > 255:
+        flash('Некорректный URL. Пожалуйста, введите действительный URL-адрес, не превышающий 255 символов.', 'danger')
+        return redirect(url_for('index'))
+
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    cursor.execute('SELECT * FROM urls WHERE name = %s', (normalized_url,))
+    if cursor.fetchone():
+        flash('Этот URL уже добавлен.', 'error')
+        cursor.close()
+        conn.close()
+        return redirect(url_for('index'))
+
+    created_at = datetime.now()
+    cursor.execute('INSERT INTO urls (name, created_at) VALUES (%s, %s)', (normalized_url, created_at))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    flash('URL успешно добавлен!', 'success')
+    return redirect(url_for('urls'))
+
+
+@app.route('/urls')
+def urls():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM urls ORDER BY created_at DESC')
+    all_urls = cursor.fetchall()
+
+    urls_list = [{'id': url[0], 'name': url[1], 'created_at': url[2]} for url in all_urls]
+
+    cursor.close()
+    conn.close()
+    return render_template('urls.html', urls=urls_list)
 
 @app.route('/urls/<int:url_id>')
 def url_detail(url_id):
-    url = url_data.get(url_id)
-    if url is None:
-        return "URL не найден", 404
-    return render_template('url_detail.html', url=url, url_id=url_id)
-
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM urls WHERE id = %s', (url_id,))
+    url_entry = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    if url_entry is None:
+        return "URL not found", 404
+    return render_template('url_detail.html', url=url_entry)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
