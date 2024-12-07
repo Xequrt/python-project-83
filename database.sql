595439d8a7b3d0cbc95c196f150648b02da31e68
Дмитрий Каблучко <xequrt@yandex.ru>
2024-12-07 22:14:19 +1000

    Create table url_checks

--- database.sql
+++ database.sql
@@ -3,3 +3,13 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id INTEGER NOT NULL REFERENCES urls(id),
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
