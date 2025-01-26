03d372a762e13f5c64f92b06653664ea43b1141a
Дмитрий Каблучко <xequrt@yandex.ru>
2025-01-27 00:13:48 +1000

    Add retries

--- page_analyzer/db_connection.py
+++ page_analyzer/db_connection.py
@@ -1,18 +1,36 @@
 import os
 from psycopg2 import pool, OperationalError
 from dotenv import load_dotenv
+import time
+
 
 load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL')
 
 
+def retry(retries=5, interval=1):
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            i = 0
+            while i < retries:
+                try:
+                    return func(*args, **kwargs)
+                except OperationalError as e:
+                    print(f"Ошибка: {e}, попытка: {i + 1}/{retries}")
+                    i += 1
+                    if i >= retries:
+                        raise
+                    time.sleep(interval)
+
+        return inner
+    return wrapper
+
+
 class Database:
     def __init__(self):
-        try:
-            self.connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
-        except OperationalError as e:
-            print(f"Ошибка при создании соединения: {e}")
+        self.connection_pool = pool.SimpleConnectionPool(1, 10, DATABASE_URL)
 
+    @retry(retries=5, interval=1)
     def get_connection(self):
         return self.connection_pool.getconn()
 
